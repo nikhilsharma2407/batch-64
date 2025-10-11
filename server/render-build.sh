bef4c488309cc336ba7165fe6bc53acbcf3f82c5
Nikhil Sharma <nikhil.sharma2407@gmail.com>
2025-10-11 15:02:42 +0530

    updated script

--- server/render-build.sh
+++ server/render-build.sh
@@ -1,15 +1,18 @@
 #!/bin/bash
 
 # Install client and build
-cd client
+cd ../client
+echo "Preparing prod client build "
 npm install
 npm run build
 
+cd ../server;
+
 # Move build to server
 echo "Removing current build";
-rm -rf ../server/dist
+rm -rf ./dist
 echo "Moving dist from client to server";
-mv dist ../server/
+mv ../client/dist ./
 
 # Install server deps
 echo "installing server dependecies";
